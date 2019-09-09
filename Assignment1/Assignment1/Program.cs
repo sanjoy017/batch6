d2407ea971d4d6c5c4eca5ed05615654168d92f3
sanjoy017 <sanjoy637r@gmail.com>
2019-09-09 18:22:30 +0600

    my_assignment_added

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter your address");
+            string add = Console.ReadLine();
+            Console.WriteLine("Enter your contact number");
+            int contact= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name + ". Your age is " + age);
+            Console.WriteLine("Your addres is "+ add +". Your contact number is "+ contact);
+
+            Console.ReadKey();
+        }
+    }
+}
